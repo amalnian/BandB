03d95a64cf33e896682273840d46d9fa0aab18f5
amalnian <amalnian@gmail.com>
2025-08-01 17:26:12 +0530

    fix 11

--- fullstack/src/components/user/chat/InboxList.jsx
+++ fullstack/src/components/user/chat/InboxList.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { InboxListItem } from "./InboxListItem";
 import { useSelector } from "react-redux";
 import { getchatconversation } from "@/endpoints/ChatAPI";
-import Conversation from "../../../pages/chat/Conversation";
+import Conversation from "../../../Pages/chat/Conversatio";
 import { useLocation, useParams } from "react-router-dom";
 
 export const InboxList = () => {
