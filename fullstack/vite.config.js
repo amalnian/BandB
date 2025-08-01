1d2a98df89002e89c4465a9cbaae9e2e5194b782
amalnian <amalnian@gmail.com>
2025-08-01 11:53:11 +0530

    fix

--- fullstack/vite.config.js
+++ fullstack/vite.config.js
@@ -6,10 +6,10 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || '/BandB/tree/main/fullstack'
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: process.env.VITE_BASE_PATH || '/BandB/tree/main/fullstack'
 })
